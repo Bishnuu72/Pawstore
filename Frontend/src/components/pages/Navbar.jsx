053f45eeda8e2cd99073871bd61d4cc722400710
Bishnu Kumar Yadav <bishnuyadav857@gmail.com>
2025-08-22 21:45:40 +0545

    fix navbar

--- Frontend/src/components/pages/Navbar.jsx
+++ Frontend/src/components/pages/Navbar.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { FiSearch } from 'react-icons/fi';
+import React from 'react'
+import { FiSearch } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#FFF1DE] px-8 py-4 flex items-center justify-between">
+    <nav className="bg-[#FFF1DE] px-20 py-10 flex items-center justify-between">
       {/* Logo */}
       <div className="flex items-center space-x-2">
         <span className="text-2xl">🐶</span>
-        <h1 className="text-lg font-semibold">Pawstore</h1>
+        <h1 className='text-lg font-semibold'>Pawstore</h1>
       </div>
 
       {/* Nav Links */}
-      <ul className="flex space-x-8 text-black font-medium">
-        <li className="font-bold">Home</li>
+      <ul className='flex space-x-8 text-black font-medium'>
+        <li>Home</li>
         <li>Breeds</li>
         <li>Accessories</li>
         <li>Blog</li>
@@ -20,16 +20,16 @@ const Navbar = () => {
       </ul>
 
       {/* Search Box */}
-      <div className="relative">
-        <input
+      <div className='relative'>
+        <input 
           type="text"
-          placeholder="Search for pets..."
-          className="pl-4 pr-10 py-2 rounded-full bg-white text-gray-600 placeholder:text-gray-400 focus:outline-none"
-        />
-        <FiSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+          placeholder='Search for pets...'
+          className='pl-4 pr-10 py-2 rounded-full bg-white text-gray-600 placeholder:text-gray-400 focus:outline-none' 
+          />
+          <FiSearch className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 ' />
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
